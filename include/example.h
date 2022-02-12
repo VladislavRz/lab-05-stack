1636f3359f5000529c563edc8f7ad3bf6250b479
VladislavRz <rzhevskii_vladislav@mail.ru>
2022-02-12 20:49:48 +0300

    New CMake

---
+++ include/example.h
@@ -0,0 +1,8 @@
+// Copyright 2022 VladislavRz <rzhevskii_vladislav@mail.ru>
+
+#ifndef LAB5_EXAMPLE_H
+#define LAB5_EXAMPLE_H
+
+void example();
+
+#endif  // LAB5_EXAMPLE_H
--- sources/example.cpp
+++ sources/example.cpp
@@ -1,1 +1,5 @@
 // Copyright 2022 VladislavRz <rzhevskii_vladislav@mail.ru>
+#include "example.h"
+#include <stdexcept>
+
+void example() { throw std::runtime_error("something wrong"); }
