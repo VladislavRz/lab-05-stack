a87faf2858aa1700a9a1ca7c86d5eeec1ee459e3
VladislavRz <rzhevskii_vladislav@mail.ru>
2022-02-12 23:56:22 +0300

    abc

--- include/example.hpp
+++ include/example.hpp
@@ -1,8 +1,8 @@
 // Copyright 2022 VladislavRz <rzhevskii_vladislav@mail.ru>
 
-#ifndef LAB5_EXAMPLE_HPP
-#define LAB5_EXAMPLE_HPP
+#ifndef INCLUDE_EXAMPLE_HPP
+#define INCLUDE_EXAMPLE_HPP
 
 auto example() -> void;
 
-#endif  // LAB5_EXAMPLE_HPP
+#endif  // INCLUDE_EXAMPLE_HPP
